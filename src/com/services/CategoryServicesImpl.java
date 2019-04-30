a84537e2615936f44c37cf04ae3b2499f473e607
Kalidas Sake <kalidasake07@gmail.com>
2019-04-30 14:54:20 +0530

    service created

---
+++ src/com/services/CategoryServicesImpl.java
@@ -0,0 +1,19 @@
+package com.services;
+
+import java.sql.ResultSet;
+
+public class CategoryServicesImpl implements CategoryService{
+
+	@Override
+	public boolean createCategory() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ResultSet getCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
